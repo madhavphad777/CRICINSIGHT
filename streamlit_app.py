4aa99ed28f75a586f50d24835b81abf3c861dbbe
madhavphad777 <122075368+madhavphad777@users.noreply.github.com>
2024-01-29 10:12:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ def set_background(url):
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # Corrected URL of your background image hosted online
-background_image_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSw8QoP4kp_b6R-aU0cKVt7uj3cIR7YWE_CVg&usqp=CAU'
+background_image_url = 'https://crickettimes.com/wp-content/uploads/2023/08/ICC-ODI-World-Cup-2023-Updated-Schedule.webp'
 
 # Call the function to set background image from the URL
 set_background(background_image_url)
